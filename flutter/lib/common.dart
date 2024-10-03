c6f1aba8e75ed9a55b6509118b1b8f40bd002c30
suxi <lovelyywhite@gmail.com>
2024-10-03 19:04:36 +0800

    lang: fluter lidesk

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -2484,7 +2484,7 @@ Future<void> onActiveWindowChanged() async {
     } catch (err) {
       debugPrintStack(label: "$err");
     } finally {
-      debugPrint("Start closing RustDesk...");
+      debugPrint("Start closing Lidesk...");
       await windowManager.setPreventClose(false);
       await windowManager.close();
       if (isMacOS) {
