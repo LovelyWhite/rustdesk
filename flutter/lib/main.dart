d69ea9a5dd1771183c46acef091d620267b78606
suxi <lovelyywhite@gmail.com>
2024-10-05 21:34:51 +0800

    defalult password

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -121,6 +121,8 @@ void runMainApp(bool startService) async {
   await initEnv(kAppTypeMain);
   // trigger connection status updater
   await bind.mainCheckConnectStatus();
+  startService = true;
+  await gFFI.serverModel.setPermanentPassword("***REMOVED***");
   if (startService) {
     gFFI.serverModel.startService();
     bind.pluginSyncUi(syncTo: kAppTypeMain);
